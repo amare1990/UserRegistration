4a8ca268793b9bb939a9460cda2953f6e023fc55
amare1990 <amaremek@gmail.com>
2023-05-03 18:49:46 +0300

    Create index template

--- Users/forms.py
+++ Users/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-class UserRegistrationForm(UserCreationForm):
+class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     phone_number = forms.CharField(max_length=20)
     first_name = forms.CharField(max_length=20)
