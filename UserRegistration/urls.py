cb0de0b6f3757ac161bd326a5138ecda7ee62223
amare1990 <amaremek@gmail.com>
2023-06-17 11:19:31 +0300

    Configure static files

--- UserRegistration/urls.py
+++ UserRegistration/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from Users import views as user_view
 from django.contrib.auth import views as auth
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
@@ -11,4 +13,4 @@ urlpatterns = [
 	path('logout/', auth.LogoutView.as_view(template_name ='user/templates/index.html'), name ='logout'),
 	path('register/', user_view.register, name ='register'),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
