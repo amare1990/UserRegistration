ff2aedec6bdf5fc36503571bc8ec11bb7915e548
amare1990 <amaremek@gmail.com>
2023-06-30 16:11:01 +0300

    Add URL patterns for password change process

--- UserRegistration/urls.py
+++ UserRegistration/urls.py
@@ -5,11 +5,16 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 urlpatterns = [
 
 	path('admin/', admin.site.urls),
 	path('', include('Users.urls')),
-	path('login/', user_view.Login, name ='login'),
+	path('login/', auth_views.LoginView, name ='login'),
 	path('logout/', auth_views.LogoutView.as_view(template_name ='user/templates/index.html'), name ='logout'),
 	path('register/', user_view.register, name ='register'),
 
@@ -18,5 +23,8 @@ urlpatterns = [
 	path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
 	path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 	path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+	path('password/change/', MyPasswordChangeView.as_view(), name='password_change'),
+  path('password/change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
